1abb4e1ab3eb79be9865104c402378453815b74f
mjjs <misiitone@gmail.com>
2018-05-19 10:00:18 +0300

    Fix linter informed errors (#12)

--- sitemappings.go
+++ sitemappings.go
@@ -1,20 +1,18 @@
 // This file is used for mapping the OS version, GPU series and GPU model into the ID values used by the Nvidia website
 package main
 
-func getOsId(windowsVersion int, sixtyFourBit bool) int {
+func getOsID(windowsVersion int, sixtyFourBit bool) int {
 	// Windows 7 and 8 / 8.1 can be grouped together, because they use the same drivers
 	if windowsVersion == 7 || windowsVersion == 8 {
 		if sixtyFourBit {
 			return 19
-		} else {
-			return 18
 		}
+		return 18
 	} else if windowsVersion == 10 {
 		if sixtyFourBit {
 			return 57
-		} else {
-			return 56
 		}
+		return 56
 	} else {
 		panic("Unsupported operating system. Either your Windows is too old (older than Windows 7) or you have errors in your config file.")
 	}
